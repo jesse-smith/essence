9c322f373adc721b48c0f77057a9e659787966a3
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-06-28 14:27:36 -0500

    Create credentials helpers

--- R/creds.R
+++ R/creds.R
@@ -5,21 +5,29 @@
 #' \code{\link[keyring:key_set]{key_set()}} and
 #' \code{\link[keyring:key_get]{key_get()}}.
 #'
-#' @param username `character`. The username for your ESSENCE account.
+#' @param user `character`. The username for your ESSENCE account.
 #'   If `NULL`, `es_creds_get()` will return the username (if set).
 #'
-#' @return `es_creds_get()` returns your username when `username = NULL` and
-#'   password otherwise. `es_creds_set()` returns `NULL`.
+#' @return `es_creds_set()` returns `NULL`. `es_creds_get()` returns the
+#' currently stored username and password as a `secret`
+#'
+#' @name es_creds
+NULL
+
+#' @rdname es_creds
 #'
 #' @export
-es_creds_set <- function(username) {
-  keyring::key_set("tn_essence", username = username)
+es_creds_set <- function(user) {
+  keyring::key_set("tn_essence", username = user)
 }
 
-es_creds_get <- function(username = NULL) {
-  if (is.null(username)) {
-    keyring::key_list("tn_essence")
-  } else {
-    keyring::key_get("tn_essence", username = username)
-  }
+#' @rdname es_creds
+#'
+#' @export
+es_creds_get <- function() {
+  user     <- keyring::key_list("tn_essence")[["username"]]
+  password <- keyring::key_get("tn_essence", username = user)
+
+  creds <- c(user = user, password = password)
+  secret(creds, secrets = c(FALSE, TRUE))
 }
