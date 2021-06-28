576ee1f07d84f36ae8cb4d989fa2b12789c665ab
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-06-28 14:21:48 -0500

    Create uri variable

---
+++ R/es_uri.R
@@ -0,0 +1,6 @@
+#' ESSENCE URI Stem
+#'
+#' @format A string
+#'
+#' @source \href{https://essence.tn.gov/tn_state/usersguide/api/documentation.jsp}{ESSENCE API Docs}
+"es_uri"
---
+++ data-raw/es_uri.R
@@ -0,0 +1,4 @@
+## code to prepare `es_uri` dataset goes here
+es_uri <- "https://essence.tn.gov/tn_state/api/"
+
+usethis::use_data(es_uri, overwrite = TRUE)
