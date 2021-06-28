e13704cdd078992c3b2b7214373897d311612f79
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-06-28 14:21:07 -0500

    Create secret class

---
+++ R/essence-package.R
@@ -0,0 +1,21 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' Internal vctrs methods
+#'
+#' @import vctrs
+#'
+#' @keywords internal
+#'
+#' @name essence-vctrs
+NULL
+
+#' @importFrom tibble %>%
+#'
+#' @export
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
